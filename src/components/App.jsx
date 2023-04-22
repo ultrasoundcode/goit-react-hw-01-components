d539fbe9c049fed70d1a16dabf24c368adc99fa2
askhat <0x.ultrasound@gmail.com>
2023-04-22 22:08:40 +0600

    Profile
    ✅

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,11 @@
+import user from '../data/user.json';
+import Profile from './Profile/Profile';
+
 export const App = () => {
   return (
     <div
       style={{
+        background: '#e1e9ee',
         height: '100vh',
         display: 'flex',
         justifyContent: 'center',
@@ -10,7 +14,13 @@ export const App = () => {
         color: '#010101',
       }}
     >
-      React homework template
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
 };
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,43 @@
+import PropTypes from 'prop-types';
+import styles from './Profile.module.css';
+
+function Profile({ username, tag, location, avatar, stats }) {
+  return (
+    <div className={styles.Profile}>
+      <div className={styles.description}>
+        <img src={avatar} alt="User avatar" className={styles.avatar} />
+        <p className={styles.name}>{username}</p>
+        <p className={styles.tag}>@{tag}</p>
+        <p className={styles.location}>{location}</p>
+      </div>
+
+      <ul className={styles.stats}>
+        <li className={styles.stats_item}>
+          <span className={styles.label}>Followers</span>
+          <span className={styles.quantity}>{stats.followers}</span>
+        </li>
+        <li className={styles.stats_item}>
+          <span className={styles.label}>Views</span>
+          <span className={styles.quantity}>{stats.views}</span>
+        </li>
+        <li className={styles.stats_item}>
+          <span className={styles.label}>Likes</span>
+          <span className={styles.quantity}>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+Profile.propTypes = {
+  username: PropTypes.string,
+  tag: PropTypes.string,
+  location: PropTypes.string,
+  avatar: PropTypes.string,
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }).isRequired,
+};
+export default Profile;
