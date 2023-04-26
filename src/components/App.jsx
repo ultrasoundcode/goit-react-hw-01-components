015b4ee8ed21b55a14c9944a6070c4a5af413e0f
askhat <0x.ultrasound@gmail.com>
2023-04-27 00:27:23 +0600

    TransactionHistory

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
-import user from '../data/user.json';
 import Profile from './Profile/Profile';
+import user from '../data/user.json';
 import Statistics from './Statistics/Statistics';
 import data from '../data/data.json';
 import FriendsList from './Friends/FriendsList';
 import friends from '../data/friends.json';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
+import transactions from '../data/transactions.json';
 
 export const App = () => {
   return (
@@ -17,6 +19,7 @@ export const App = () => {
       />
       <Statistics title="Upload stats" stats={data} />
       <FriendsList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
