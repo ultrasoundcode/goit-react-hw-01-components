d4124c8f888387b523b1211d2ad9c568521ea8b4
askhat <0x.ultrasound@gmail.com>
2023-04-28 23:03:05 +0600

    update

--- src/camponents/FriendList/FriendsList.styled.js
+++ src/camponents/FriendList/FriendsList.styled.js
@@ -8,4 +8,9 @@ export const Friends = styled.ul`
   flex-direction: column;
   padding: 0;
   margin-top: 30px;
+  transition: transform 500ms;
+  cursor: pointer;
+  :hover {
+    transform: scale(1.05);
+  }
 `;
